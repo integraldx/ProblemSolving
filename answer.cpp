37005ccf1583bb18c11898a4a14e92df7218b1c2
integraldx <jmh121463@gmail.com>
2019-07-31 18:58:11 +0900

    Initial Commit

---
+++ answer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <utility>
+#include <algorithm>
+#include <set>
+#include <list>
+
+using namespace std;
+
+void init()
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+}
+
+int main()
+{
+    init();
+
+    cout.flush();
+    return 0;
+}
